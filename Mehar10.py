5d1b9a06206f4f898831de3ef25fb6d9650f305f
Mehar6 <76868439+Mehar6@users.noreply.github.com>
2021-01-13 22:43:41 +0500

    Update Mehar10.py

--- Mehar10.py
+++ Mehar10.py
@@ -282,7 +282,7 @@ def setupVNC(ngrok_region = None, check_gpu_available = True):
   print(msg)
 
 def apps():
- ! sudo add-apt-repository ppa:obsproject/obs-studio -y
+  sudo add-apt-repository ppa:obsproject/obs-studio -y
   clear_output()
   my_apt = _MyApt()
   my_apt.installPkg("obs-studio", "vlc","chrome",  "firefox", "xarchiver", "xfce4", "xfce4-goodies")
